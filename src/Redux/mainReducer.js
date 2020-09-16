a1760e8f46f1dd6337d99172e17295965b615b8a
Artem <april.tim21@gmail.com>
2020-09-16 17:12:22 +0300

    fix sort

--- src/Redux/mainReducer.js
+++ src/Redux/mainReducer.js
@@ -18,11 +18,11 @@ const initialState = {
     small: 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
     big: 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
   },
-  selectDataType:'',
+  selectDataType: '',
 };
 
-const mainReducer = ( state = initialState, action ) => {
-  switch ( action.type ) {
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
     case GET_DATA:
       return {
         ...state,
@@ -38,14 +38,23 @@ const mainReducer = ( state = initialState, action ) => {
       return {
         ...state,
         dataSort: [
-          ...state.dataSort.sort( ( a, b ) =>
-            state.sortType
-              ? a[ action.payload ] > b[ action.payload ]
-                ? 1
-                : -1
-              : b[ action.payload ] > a[ action.payload ]
-                ? 1
-                : -1
+          ...state.dataSort.sort((a, b) =>
+            (typeof a[action.payload] === 'string' && typeof b[action.payload] === 'string')
+              ? state.sortType
+                ? a[action.payload].toLowerCase() > b[action.payload].toLowerCase()
+                  ? 1
+                  : -1
+                : b[action.payload].toLowerCase() > a[action.payload].toLowerCase()
+                  ? 1
+                  : -1
+              :
+              state.sortType
+                ? a[action.payload] > b[action.payload]
+                  ? 1
+                  : -1
+                : b[action.payload] > a[action.payload]
+                  ? 1
+                  : -1
           ),
         ],
         sortType: !state.sortType,
@@ -54,18 +63,18 @@ const mainReducer = ( state = initialState, action ) => {
     case ADD_USER:
       return {
         ...state,
-        data: [ action.payload, ...state.data ],
-        dataSort: [ action.payload, ...state.data ],
+        data: [action.payload, ...state.data],
+        dataSort: [action.payload, ...state.data],
       };
     case ON_SEARCH:
       return {
         ...state,
         search: action.payload,
         dataSort: action.payload
-          ?  state.data.filter(user => user['firstName'].toLowerCase().includes( action.payload.toLowerCase() ) ||
-                user['lastName'].toLowerCase().includes( action.payload.toLowerCase() ) ||
-                user['email'].toLowerCase().includes( action.payload.toLowerCase() ))
-          :  [...state.data]
+          ? state.data.filter(user => user['firstName'].toLowerCase().includes(action.payload.toLowerCase()) ||
+                        user['lastName'].toLowerCase().includes(action.payload.toLowerCase()) ||
+                        user['email'].toLowerCase().includes(action.payload.toLowerCase()))
+          : [...state.data]
       };
     case SET_DATA_TYPE:
       return {
@@ -75,29 +84,29 @@ const mainReducer = ( state = initialState, action ) => {
     case SET_PAGE_USER_COUNT:
       return {
         ...state,
-        dataSort: state.data.filter( ( obj, index ) => (
-          action.payload.lowerLimit < index + 1 && index + 1 <= action.payload.upperLimit )),
+        dataSort: state.data.filter((obj, index) => (
+          action.payload.lowerLimit < index + 1 && index + 1 <= action.payload.upperLimit)),
       };
     default:
       return state;
   }
 };
 
-export const getData = data => ({ type: GET_DATA, payload: data });
-export const getUserInfo = user => ({ type: GET_USER_INFO, payload: user });
+export const getData = data => ({type: GET_DATA, payload: data});
+export const getUserInfo = user => ({type: GET_USER_INFO, payload: user});
 export const sortColumn = category => ({
   type: SORT_COLUMN,
   payload: category,
 });
-export const addUser = newUser => ({ type: ADD_USER, payload: newUser });
-export const onSearch = search => ({ type: ON_SEARCH, payload: search });
+export const addUser = newUser => ({type: ADD_USER, payload: newUser});
+export const onSearch = search => ({type: ON_SEARCH, payload: search});
 export const setDataType = dataType => ({
   type: SET_DATA_TYPE,
   payload: dataType,
 });
-export const setPageUserCount =( lowerLimit, upperLimit) => ({
+export const setPageUserCount = (lowerLimit, upperLimit) => ({
   type: SET_PAGE_USER_COUNT,
-  payload: { lowerLimit, upperLimit }
+  payload: {lowerLimit, upperLimit}
 });
 
 export default mainReducer;
